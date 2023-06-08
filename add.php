a41fae3ca53ba92f1d6121bf98b367243c382180
KarelNurm <75736496+KarelNurm@users.noreply.github.com>
2023-06-08 08:50:17 +0300

    Update add.php

--- add.php
+++ add.php
@@ -10,43 +10,59 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="fancybox/jquery.fancybox.css">
 
-<style>
-
-
-</style>
 
   </head>
   <body>
-
+<div class="container">
   <?php
 
- $artist = htmlspecialchars(trim($_GET['Nimi']));
- $album = htmlspecialchars(trim($_GET['Perekonnanimi']));
- $aasta = htmlspecialchars(trim($_GET['Võistlusklass']));
- $hind = htmlspecialchars(trim($_GET['Email']));
- $paring = "INSERT INTO Muusikapood(artist,album,aasta,hind) VALUES ('".$Nimi."','".$Perekonnanimi."','".$Võistlusklass."','".$Email."')";
- $valjund = mysqli_query($yhendus, $paring);
- //pĆ¤ringu vastuste arv
- $tulemus = mysqli_affected_rows($yhendus);
- if ($tulemus == 1) {
-  echo '<META HTTP-EQUIV="Refresh" Content="0; URL=admin.php">';
- } else {
- echo "Kirjet ei lisatud";
- }
+$nimi = htmlspecialchars(trim($_GET['nimi']));
+ $pere = htmlspecialchars(trim($_GET['pere']));
+ $klass = htmlspecialchars(trim($_GET['klass']));
+ $email = htmlspecialchars(trim($_GET['email']));
+
+// Kontrollime, kas kõik väljad on täidetud
+if ($nimi && $pere && $klass && $email) {
+    // Kontrollime, kas kasutaja on juba registreeritud
+    $kontroll_paring = "SELECT * FROM uuemoisa WHERE email = '$email'";
+    $tulemus = $yhendus->query($kontroll_paring);
+
+    if ($tulemus->num_rows > 0) {
+        echo "Kasutaja e-posti aadress '$email' on juba registreeritud.";
+    } else {
+        // Registreerime kasutaja
+        $paring = "INSERT INTO uuemoisa (nimi, pere, klass, email) VALUES ('$nimi', '$pere', '$klass', '$email')";
+        if ($yhendus->query($paring) === true) {
+            echo "Kasutaja '$nimi $pere $klass' on edukalt registreeritud.";
+        } else {
+            echo "Kasutaja registreerimine ebaõnnestus: " . $yhendus->error;
+        }
+    }
+} else
  //Ć¼henduse sulgemine
  mysqli_close($yhendus); 
 ?>
-<h2>Uue albumi lisamine</h2>
-<form action="" method="get" enctype="multipart/form-data">
-<table>
-    <tr><td>Artist: </td><td><input type="text" name="artist" required></td></tr>
-    <tr><td>Album:</td><td> <input type="text" name="album" required></td></tr>
-    <tr><td>Aasta: </td><td><input type="number" name="aasta" value="2000" min="1900" max="2099" required></td></tr>
-
-    <tr><td>Hind: </td><td><input type="number" name="hind" value="1" min="0.01" max="10000000000000000" step="0.1" required></td></tr>
-    <tr><td><input type="submit" value="Lisa uus album"></td><td><input type="reset" value="Tühjenda"></td></tr>
-</table>
-</form>
+<h2>Võistleja lisamine</h2>
+        <!-- Registration Form -->
+        <form>
+            <div class="form-group">
+                <label >Eesnimi</label>
+                <input type="text" class="form-control" name="nimi" required>
+            </div>
+            <div class="form-group">
+                <label >Perenimi</label>
+                <input type="text" class="form-control" name="pere" required>
+            </div>
+            <div class="form-group">
+                <label >Võistlusklass</label>
+                <input type="number" class="form-control" name="klass" value="1" min="1" max="100" required>
+            </div>
+            <div class="form-group">
+                <label >Email</label>
+                <input type="text" class="form-control" name="email" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Lisa uus võistleja</button>
+        </form>
 </div>
 </body>
 </html>
